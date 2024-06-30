4eb1f9916df197575f1381cae232ae87ae57de7d
Ayushirani03 <150169944+Ayushirani03@users.noreply.github.com>
2024-06-30 20:41:46 +0530

    Add files via upload

---
+++ intro.java
@@ -0,0 +1,9 @@
+class Intro{
+public static void main(String[] args) {
+    System.out.println("myself ayushi rani");
+    System.out.println("i'm a bsc graduate and recently pursuing mca from AUJ");
+    System.out.println("i am from hazaribagh");
+    System.out.println("my skills are java,dbms,os,html,css");
+    System.out.println("my hobbies are dancing and travelling");
+}
+}
