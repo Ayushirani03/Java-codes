cbf3e49c408adf5f1110e74c49792c1c9e20320c
Ayushirani03 <150169944+Ayushirani03@users.noreply.github.com>
2024-07-01 16:08:45 +0530

    Add files via upload

---
+++ Circum.java
@@ -0,0 +1,8 @@
+public class Circum {
+    public static void main(String[] args) {
+        float a=3.0f;
+        float pi=3.14f;
+        float peri=2*pi*a;
+        System.out.println("circumference of the circle is:" + peri);
+    }
+}
