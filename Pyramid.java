523d8f347c6ff0e164b3d28f9856a0c7489ac6e6
Ayushirani03 <150169944+Ayushirani03@users.noreply.github.com>
2024-08-09 23:55:01 +0530

    Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -5,7 +5,7 @@ public class Pyramid {
                 System.out.print(" ");
             }
             for(int j=1;j<=i;j++){
-                System.out.print(" * ");
+                System.out.print("* ");
             }
             System.out.println();
         }
